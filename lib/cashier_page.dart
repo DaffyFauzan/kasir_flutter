e5bc3246e00d0df918fc472e985e2e86882e38d9
Ramon hidayat <hidayatmramon@gmail.com>
2024-11-04 09:58:13 +0700

    setup

---
+++ lib/cashier_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CashierPage extends StatefulWidget {
+  const CashierPage({super.key});
+
+  @override
+  State<CashierPage> createState() => _CashierPageState();
+}
+
+class _CashierPageState extends State<CashierPage> {
+  @override
+
+  Widget build(BuildContext context){
+    return Scaffold();
+  }
+}
