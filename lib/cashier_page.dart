c10256b678f6f94a980b1e3b0dac43bf3b86c673
Ramon hidayat <hidayatmramon@gmail.com>
2024-11-18 13:27:44 +0700

    dynamic stoc

--- lib/cashier_page.dart
+++ lib/cashier_page.dart
@@ -15,155 +15,261 @@ class _CashierPageState extends State<CashierPage> {
       'name': 'Coklat Susu',
       'price': 10000,
       'stock': 10,
-      'imageUrl': 'https://png.pngtree.com/png-vector/20240203/ourmid/pngtree-chocolate-drink-png-png-image_11532197.png'
+      'imageUrl':
+          'https://png.pngtree.com/png-vector/20240203/ourmid/pngtree-chocolate-drink-png-png-image_11532197.png',
     },
     {
       'name': 'Kopi Latte',
       'price': 15000,
       'stock': 10,
-      'imageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfX9w9kqyrS8UwWi99xMTiv_cV-BZk1vnKtg&s'
+      'imageUrl':
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfX9w9kqyrS8UwWi99xMTiv_cV-BZk1vnKtg&s',
     },
   ];
+
+  int _totalItem = 0;
+  int _totalHarga = 0;
+
+  Future<void> _TambahItemBeli(int index) async {
+    setState(() {
+      if (products[index]['stock'] > 0) {
+        products[index]['stock']--;
+        products[index]['quantity'] = (products[index]['quantity'] ?? 0) + 1;
+        _totalItem++;
+        _totalHarga += products[index]['price'] as int;
+      }
+    });
+  }
+
+  Future<void> _KurangItemBeli(int index) async {
+    setState(() {
+      if (products[index]['quantity'] != null &&
+          products[index]['quantity'] > 0) {
+        products[index]['stock']++;
+        products[index]['quantity'] = products[index]['quantity'] - 1;
+        _totalItem--;
+        _totalHarga -= products[index]['price'] as int;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(
-          top: 50,
-          left: 20,
-          right: 20,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              "Kasirku",
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.w300,
-              ),
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 50,
+              left: 20,
+              right: 20,
             ),
-            const Text(
-              "Semoga Harimu Menyenangkan :)",
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              controller: searchController,
-              decoration: InputDecoration(
-                hintText: 'Cari produk...',
-                hintStyle: TextStyle(
-                  color: Colors.grey[400],
-                ),
-                border: OutlineInputBorder(
-                  borderSide: const BorderSide(
-                    color: Colors.black,
-                  ),
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8.0),
-                  borderSide: const BorderSide(
-                    color: Colors.black,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Cashier App",
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.w300,
                   ),
                 ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            Expanded(
-              child: ListView.separated(
-                separatorBuilder: (context, index) {
-                  return const SizedBox(
-                    height: 15,
-                  );
-                },
-                itemCount: products.length,
-                itemBuilder: (context, index) {
-                  final product = products[index];
-                  return Container(
-                    height: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8.0),
-                      color: Colors.grey[300],
+                const Text(
+                  "Semoga Harimu Menyenangkan :D",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: searchController,
+                  decoration: InputDecoration(
+                    hintText: 'Cari Produk...',
+                    hintStyle: TextStyle(
+                      color: Colors.grey[400],
                     ),
-                    child: Row(
-                      children: [
-                        Container(
+                    border: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.black),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: const BorderSide(color: Colors.black),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 100),
+                    child: ListView.separated(
+                      separatorBuilder: (context, index) =>
+                          const SizedBox(height: 15),
+                      itemCount: products.length,
+                      itemBuilder: (context, index) {
+                        return Container(
                           height: 100,
-                          width: 100,
                           decoration: BoxDecoration(
-                            borderRadius: const BorderRadius.only(
-                              topLeft: Radius.circular(8.0),
-                              bottomLeft: Radius.circular(8.0),
-                            ),
-                            color: Colors.grey[400],
+                            borderRadius: BorderRadius.circular(8.0),
+                            color: Colors.grey[300],
                           ),
-                          child: ClipRRect(
-                            borderRadius: const BorderRadius.only(
-                              topLeft: Radius.circular(8.0),
-                              bottomLeft: Radius.circular(8.0),
-                            ),
-                            child: Image.network(
-                              product['imageUrl'],
-                              fit: BoxFit.cover,
-                              errorBuilder: (context, error, stackTrace) {
-                                return const Icon(Icons.image_not_supported);
-                              },
-                            ),
-                          ),
-                        ),
-                        const SizedBox(
-                          width: 20,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 10),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                          child: Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
+                              Row(
                                 children: [
-                                  Text(
-                                    product['name'],
-                                    style: const TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w400,
+                                  Container(
+                                    height: 100,
+                                    width: 100,
+                                    decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                        image: NetworkImage(
+                                            products[index]['imageUrl']),
+                                        fit: BoxFit.cover,
+                                      ),
+                                      borderRadius: const BorderRadius.only(
+                                        topLeft: Radius.circular(8.0),
+                                        bottomLeft: Radius.circular(8.0),
+                                      ),
+                                      color: Colors.grey[400],
                                     ),
                                   ),
-                                  Text(
-                                    "Stock: ${product['stock']}",
-                                    style: const TextStyle(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w300,
+                                  const SizedBox(width: 10),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 10),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          "${products[index]['name']}",
+                                          style: const TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w400,
+                                          ),
+                                        ),
+                                        Text(
+                                          "Stock = ${products[index]['stock']}",
+                                          style: const TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w400,
+                                          ),
+                                        ),
+                                        Text(
+                                          "Rp. ${products[index]['price']}",
+                                          style: const TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w400,
+                                          ),
+                                        ),
+                                      ],
                                     ),
                                   ),
                                 ],
                               ),
-                              Text(
-                                "Rp. ${product['price']}",
-                                style: const TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w400,
+                              Padding(
+                                padding: const EdgeInsets.only(right: 10),
+                                child: Row(
+                                  children: [
+                                    Visibility(
+                                      visible: products[index]['quantity'] !=
+                                              null &&
+                                          products[index]['quantity'] > 0,
+                                      child: GestureDetector(
+                                        onTap: () => _KurangItemBeli(index),
+                                        child: const SizedBox(
+                                          height: 30,
+                                          width: 30,
+                                          child: Icon(
+                                            Icons
+                                                .remove_circle_outline_rounded,
+                                            color: Colors.red,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: 40,
+                                      child: Center(
+                                        child: Visibility(
+                                          visible: products[index]['quantity'] !=
+                                                  null &&
+                                              products[index]['quantity'] > 0,
+                                          child: Text(
+                                            "${products[index]['quantity']}",
+                                            style: const TextStyle(
+                                              fontWeight: FontWeight.w500,
+                                              fontSize: 16,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    GestureDetector(
+                                      onTap: () => _TambahItemBeli(index),
+                                      child: const SizedBox(
+                                        height: 30,
+                                        width: 30,
+                                        child: Icon(
+                                          Icons.add_circle_outline_outlined,
+                                          color: Colors.green,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ),
                             ],
                           ),
-                        )
-                      ],
+                        );
+                      },
                     ),
-                  );
-                },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: InkWell(
+              onTap: () {
+                // Handle checkout action here
+              },
+              child: Container(
+                margin: const EdgeInsets.all(20),
+                height: 55,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Total ($_totalItem item) = Rp. $_totalHarga",
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const Icon(
+                        Icons.shopping_cart_checkout_outlined,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
